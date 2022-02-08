749be57be8dd4b92c412bf84925ea9d11da8115a
Suzanne Rozier <suz@truss.works>
2022-02-08 09:44:27 -0600

    Set up jest, add a basic test

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  roots: ['<rootDir>', '<rootDir>/src/'],
+  testPathIgnorePatterns: ['/node_modules/', '.keystone/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(.keystone)/)'],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
+}
---
+++ src/schema.test.ts
@@ -0,0 +1,30 @@
+import { setupTestRunner } from '@keystone-6/core/testing'
+import { config } from '@keystone-6/core'
+
+import { lists } from './schema'
+
+describe('Keystone schema', () => {
+  const testConfig = config({
+    db: { provider: 'sqlite', url: 'file:./test-cms.db' },
+    lists,
+  })
+
+  const runner = setupTestRunner({ config: testConfig })
+
+  describe('Users', () => {
+    it('can create a user', () => {
+      runner(async ({ context }) => {
+        const user = await context.query.User.createOne({
+          data: {
+            name: 'Test User',
+            email: 'testuser@example.com',
+            password: 'testuserpassword',
+          },
+          query: 'id name email',
+        })
+
+        expect(user).toBeTruthy()
+      })
+    })
+  })
+})
