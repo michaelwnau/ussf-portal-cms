9a356f628365d30817a92e60081ef30e58aba252
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2022-05-16 14:10:23 -0500

    chore: updating rds cert for us-west-gov-1 (#46)

--- scripts/add-rds-cas.sh
+++ scripts/add-rds-cas.sh
@@ -3,6 +3,8 @@
 
   comcert=https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
   govcert=https://s3-us-gov-west-1.amazonaws.com/rds-downloads/rds-combined-ca-us-gov-bundle.pem
-  
+  uswestgov=https://truststore.pki.us-gov-west-1.rds.amazonaws.com/us-gov-east-1/us-gov-west-1-bundle.pem
+
   wget $comcert
   wget $govcert
+  wget $uswestgov
