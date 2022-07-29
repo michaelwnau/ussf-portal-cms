1fe6385a8a3d7bad82ef9e38e1d021ba40a2c9d0
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2022-07-29 16:48:20 -0500

    chore: adding a script to snapshot RDS prior to a deploy (#99)
    * docs: adding a how-to delete docdb snapshots
    
    * chore: do the actual thing
    
    * chore: typo and new line to appease the linters.

---
+++ scripts/snapshot-rds.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+set -e
+
+# Snapshot ID `rdsss-epoch`
+SSID=rdsss-$(date +"%s")
+
+# Create Snapshot
+aws rds create-db-snapshot \
+  --db-instance-identifier ${RDS_INSTANCE} \
+  --db-snapshot-identifier $SSID --tags Key=sha,Value=${IMAGE_TAG}
+
+# RDS Snapshot
+RDSSS=$(aws rds describe-db-snapshots --db-snapshot-identifier $SSID |jq -r '.DBSnapshots[]' | jq -r '.Status')
+
+# Don't do anything else until it's available
+# aws docdb does not have a `wait`
+until [ "$DBSS" = "available" ]
+    do
+      DBSS=$(aws rds describe-db-snapshots --db-snapshot-identifier $SSID |jq -r '.DBSnapshots[]' | jq -r '.Status')
+      echo $RDSSS
+      sleep 30
+    done
+    echo "$SSID available!"
